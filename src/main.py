a0ead35fb82ef4e023d1091cea95008f2cc03085
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-08-08 17:13:03 +0800

    organize code

---
+++ src/main.py
@@ -0,0 +1,1 @@
+import readMySQL, removeHTML
---
+++ src/readMySQL.py
@@ -0,0 +1,20 @@
+# You have to install MySQLdb first
+import MySQLdb
+
+def readMySQL():
+	db = MySQLdb.connect(host="localhost", # your host, usually localhost
+	                     user="root", # your username
+	                     passwd="ydh0114", # your password
+	                     db="stackoverflow201503") # name of the data base
+
+	# you must create a Cursor object. It will let
+	#  you execute all the queries you need
+	cur = db.cursor() 
+
+	# Use all the SQL you like
+	cur.execute("SELECT * FROM posts where Id=18553292 or ParentId=18553292")
+
+	# print all the first cell of all the rows
+	#for row in cur.fetchall() :
+	#    print row[0]
+

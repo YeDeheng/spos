b3f09adb14ebd1b05256154ac88e1c5430b43303
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-08-20 21:06:38 +0800

    done.

--- src/main.py
+++ src/main.py
@@ -10,27 +10,28 @@ import sys,os
 
 def wrapper(istring, ostring):
 	cur_dir = os.getcwd()
-	batch_path = cur_dir + r'\batch.bat'
-	
+	batch_path = cur_dir + r'\pretag\pretag.bat'  # this is the batch file
+	print batch_path
+
 	os.chdir(".\data_final")
 
 	q_id = istring
 	q_raw_content = q_id + '-raw.txt'
 	q_tokenize = q_id + '-tokenized.txt'
-	q_in = q_id + '-input2tagger.txt'
+	q_transformin = q_id + '-input2tagger.txt'
 	q_pretag = q_id + '-pretagged.txt'
 
 	readSQL.read_knowledge_unit(q_id, q_raw_content)
 	tokenize.tokenize(q_raw_content, q_tokenize)
-	transforminput.transforminput(q_tokenize, q_in)
+	transforminput.transforminput(q_tokenize, q_transformin)
 
+	os.chdir("..\pretag")
 
-	child = subprocess.Popen(batch_path)
+	child = subprocess.Popen([batch_path, "..\\data_final\\" + q_transformin, "..\\data_final\\" + q_pretag])
 	stdout, stderr = child.communicate()
-	child.wait()
-	
-	#print("parent process")
-	
+	child.wait()	# wait() is important. 
+	print("parent process")
+
 	os.chdir("..")
 
 if __name__=='__main__':
--- src/wrapper.bat
+++ src/wrapper.bat
@@ -2,5 +2,8 @@
 :: usage: stanford-postagger model textFile
 ::  e.g., stanford-postagger models\english-left3words-distsim.tagger sample-input.txt
 :: pre-tag the input text using stanford Tagger
+::java -mx300m -cp "stanford_tagger\stanford-postagger-3.5.2.jar;" edu.stanford.nlp.tagger.maxent.MaxentTagger -sentenceDelimiter newline -tokenize false -model stanford_tagger\models\wsj-0-18-left3words-distsim.tagger -textFile input2tagger.txt > pretag.txt
 
-java -mx300m -cp "stanford_tagger\stanford-postagger-3.5.2.jar;" edu.stanford.nlp.tagger.maxent.MaxentTagger -sentenceDelimiter newline -tokenize false -model stanford_tagger\models\wsj-0-18-left3words-distsim.tagger -textFile input2tagger.txt > pretag.txt
+
+cd .\pretag
+pretag.bat ..\data_final\

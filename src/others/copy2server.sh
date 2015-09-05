88bfa6c9a4c88a092141dd616fa21116da9cf2e7
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-09-05 11:58:12 +0800

    annotator

--- src/others/copy2server.sh
+++ src/others/copy2server.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 cd /var/www/html/stackoverflow-annotator/
-for user in lingfeng gaosa zhenchang xuejiao chunyang lijing
+for user in wentao #liuyi #ziqun #lingfeng gaosa zhenchang xuejiao chunyang lijing
 do
 	cd $user
 	for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
---
+++ stackoverflow-annotator/info.php
@@ -0,0 +1,10 @@
+<?php
+/* Set internal character encoding to UTF-8 */
+mb_internal_encoding("UTF-8");
+
+/* Display current internal character encoding */
+echo mb_internal_encoding();
+
+
+phpinfo();
+?> 

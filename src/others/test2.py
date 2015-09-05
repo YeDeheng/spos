1d2e39c6f7088065a164237f797cf49fba6c977e
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-09-05 10:53:15 +0800

    .

--- src/others/test2.py
+++ src/others/test2.py
@@ -1,5 +1,28 @@
-import re
+# from HTMLParser import HTMLParser
+# from bs4 import BeautifulSoup
+# class MLStripper(HTMLParser):
+#     def __init__(self):
+#         self.reset()
+#         self.fed = []
+#     def handle_data(self, d):
+#             self.fed.append(d)
+#     def get_data(self):
+#         return ' '.join(self.fed)
+        
+# def strip_tags(html):
+#     s = MLStripper()
+#     s.feed(html)
+#     return s.get_data()
 
-text = 'http://asfas.com/asdfa/ '
-text = re.sub(r'(?<=\w)/(?=\s+)', ' ', text)
-print text
+
+# html = '&abc= is my input text'
+# soup = BeautifulSoup(html)
+# print soup.text
+# print strip_tags('&abc= is my input text')
+
+from bs4 import BeautifulSoup
+
+html = '&abc= is my input text<h2>asdf</h2>'
+soup = BeautifulSoup(html, 'html.parser')
+all_text = ' '.join(soup.findAll(text=True))
+print all_text

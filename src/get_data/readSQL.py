45332c4116be4fdd7e9a9baddc08a62c12af8bd1
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-09-06 09:32:18 +0800

    a more robust html parser.

--- src/get_data/readSQL.py
+++ src/get_data/readSQL.py
@@ -4,6 +4,7 @@ import sys,os
 import MySQLdb
 import re
 from HTMLParser import HTMLParser
+from htmlentitydefs import entitydefs
 
 import codecs
 codecs.register_error('replace_against_space', lambda e: (u' ',e.start + 1))
@@ -13,11 +14,37 @@ class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
         self.fed = []
+        self.entityref = re.compile('&[a-zA-Z][-.a-zA-Z0-9]*[^a-zA-Z0-9]')
+    
     def handle_data(self, d):
         self.fed.append(d)
+
+    def handle_starttag(self, tag, attrs):
+        self.fed.append(' ')
+
+    def handle_endtag(self, tag):
+        self.fed.append(' ')
+
+    def handle_entityref(self, name):
+        if entitydefs.get(name) is None:
+            m = self.entityref.match(self.rawdata.splitlines()[self.lineno-1][self.offset:])
+            entity = m.group()
+            # semicolon is consumed, other chars are not.
+            if entity[-1] != ';':
+                entity = entity[:-1]
+            self.fed.append(entity)
+        else:
+            self.fed.append(' ')
+
     def get_data(self):
-        #return self.fed
-        return ' '.join(self.fed)
+        self.close()
+        return ''.join(self.fed)
+
+    # def handle_data(self, d):
+    #     self.fed.append(d)
+    # def get_data(self):
+    #     #return self.fed
+    #     return ' '.join(self.fed)
 
 def strip_tags(html):
     s = MLStripper()

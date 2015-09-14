5a8c859452139b58f394652705a02047b5bed630
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-09-14 15:55:28 +0800

    finally...

--- src/baseline1/compare-output-golden.py
+++ src/baseline1/compare-output-golden.py
@@ -1,6 +1,4 @@
-
 mapped_tag_list = ['O', 'N', '^', 'V', 'A', 'R', '!', 'D', 'P', 'T', 'X', '&', '$', 'G']
-
 def compare(istring1, istring2):
 	test = open(istring1, 'r')
 	gold = open(istring2, 'r')
@@ -24,12 +22,6 @@ def compare(istring1, istring2):
 					equals += 1
 	print (equals, total)
 	return equals/float(total)
-
-
-
-
-
-
 def transform_golden(istring, ostring):
 	ifile = open(istring, 'r')
 	ofile = open(ostring, 'w')
@@ -40,11 +32,8 @@ def transform_golden(istring, ostring):
 		tags = elem[1].split(' ')
 		for token, tag in zip(tokens, tags):
 			ofile.write(token+'\t'+tag+'\n')
-			
-
-
 if __name__=='__main__':
-	transform_golden('sample-golden.txt', 'golden_tran.txt')
-	#print compare('sample-output.txt', 'golden_tran.txt')	
+	#transform_golden('sample-golden.txt', 'golden_tran.txt')
+	print compare('sample-output.txt', 'golden_tran.txt')	
 	# #transform_golden('zhenchang5.automatic_tags', 'golden_tran.txt')
 	# print compare('zhenchang5.automatic_tags.ST.mapped', 'golden_tran.txt')
